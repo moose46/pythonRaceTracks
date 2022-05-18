fd69ef274c23caa11569e5c7a534c3008902b481
Robert Curtiss <me@robertcurtiss.com>
2022-05-17 20:45:54 -0400

    Read race result files and bets file

--- read_bet_file.py
+++ read_bet_file.py
@@ -5,6 +5,7 @@ import csv
 from collections import namedtuple
 from datetime import date
 from pathlib import Path
+import read_data_file
 
 """
 ====================================================
@@ -32,24 +33,33 @@ class RaceBet:
     greg_selected: str
     bob_finish: int = -1
     greg_finish: int = -1
+    _data_file_name: str   # name of the data file
 
     @property
     def date_of_race(self):
         """Race year"""
         return self._date_of_race
 
+    @property
+    def data_file_name(self) -> Path:
+        self._data_file_name = self._date_of_race.strftime('%Y%m%d')
+
+        the_path = Path.cwd() / 'data' / f'{self.race_track}' / f'{self._data_file_name}.txt'
+        return the_path
+
+
     @date_of_race.setter
     def date_of_race(self, d):
-        """ Expected format: month/day/year"""
-        month,day,year = d.split('/')
-        self._date_of_race = date(int(year),int(month),int(day))
+        """Expected format: month/day/year"""
+        month, day, year = d.split('/')
+        self._date_of_race = date(int(year), int(month), int(day))
 
     def __repr__(self):
         return (
             f'{self.race_track.upper():<12} {self.date_of_race.strftime("%m/%d/%Y"):<10}'
             f' BOB: {self.bob_finish:<2} {self.bob_selected.capitalize():16}'
             f' GREG: {self.greg_finish:<2} {self.greg_selected.capitalize():16}'
-
+            f' fname={self.data_file_name}'
         )
 
 
@@ -79,4 +89,6 @@ def get_data(file_name: str = 'bets.txt') -> [RaceBet]:
 
 r = get_data()
 for x in r:
-    print(x)
+    d = read_data_file.read_the_data_file(data_file_name=x.data_file_name)
+    for i in d:
+        print(i)
---
+++ read_data_file.py
@@ -0,0 +1,80 @@
+__author__ = 'Robert W. Curtiss'
+__project__ = 'Python Basics'
+
+import csv
+import os
+from collections import namedtuple
+from pathlib import Path
+
+"""
+====================================================
+Author: Robert W. Curtiss
+    Project: 
+    File: read_data_file
+    Created: May, 15, 2022
+    
+    Description:
+        returns a list of named tuples for all the race dates
+        for a track    
+===================================================
+"""
+
+class DriverRaceResults:
+    """
+    pos = finish position
+    driver = diver name
+    manufacturer = car manufacturer
+    """
+    def __init__(self, year: str = ''):
+        self._year: str = year
+
+    pos: int
+    driver: str
+    manufacturer: str
+    car: int
+
+    @property
+    def year(self):
+        """ Race year """
+        return self._year
+    def __repr__(self):
+        return f'{self.driver} {self.pos}'
+
+def read_the_data_file(data_file_name: Path) -> [DriverRaceResults]:
+    """
+    Gets all data for a track from the data/track name directory
+    driver, car, manufacturer,laps,start,led,pts,bonus, penalty
+    :param track_name:
+    :return: list of DriverRaceResults
+    """
+    data: [DriverRaceResults] = list()
+    if not os.path.isdir(data_file_name.parent):
+        os.mkdir(data_file_name.parent)
+    if not os.path.exists(data_file_name):
+        return data
+    with open(str(data_file_name), 'r') as file:
+        reader = csv.reader(file, delimiter='\t')
+        row_data = namedtuple('result', next(reader), rename=True)
+        for row in reader:
+            results_data = row_data(*row)  # results_data is a tuple
+            fc = DriverRaceResults()
+            (
+                fc.pos,
+                fc.driver,
+                fc.car,
+                fc.manufacturer,
+                _,
+                _,
+                _,
+                _,
+                _,
+                _,
+            ) = row_data(*row)
+                # results_data.BONUS = f.name[:4] # put the year in the bonus field
+            data.append(fc)
+    return data
+
+
+# r = get_data()
+# for x in r:
+#     print(x)
